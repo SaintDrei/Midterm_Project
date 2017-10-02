2704b88ffb00eb210767ebeb827015b65a7ddf79
Andrei Santos <andreimishael.santos@benilde.edu.ph>
2017-10-02 19:37:51 +0800

    Added Trails
    Trails for instruction trails.

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -16,3 +16,31 @@ Route::get('hello', function(){
 });
 
 Route::
+
+
+
+    ?>
+<html>
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+    <div class="container">
+        <div class="card col l12 m12 s12">
+            <h1>M<y Own Laravel Website</h1>
+            <div class="jumbotron">
+                <p>This is my own Laravel Website, in Materialize!</p>
+            </div>
+        </div>
+    </div>
+<body>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+</body>
+</html>
